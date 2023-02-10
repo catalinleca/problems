2531fb0ab719df882269daa7829fb9f4ef2babd8
Catalin Leca <leca.catalin1995@gmail.com>
2023-02-10 12:25:53 +0200

    with sort

--- src/sameSqrtElementsArray.js
+++ src/sameSqrtElementsArray.js
@@ -38,3 +38,10 @@ function comp(array1, array2){
 
   return !Object.values(map).some( x => x !== 0 )
 }
+
+// with sort
+function comp2(array1, array2) {
+  if(array1 == null || array2 == null) return false;
+  array1.sort((a, b) => a - b); array2.sort((a, b) => a - b);
+  return array1.map(v => v * v).every((v, i) => v == array2[i]);
+}
