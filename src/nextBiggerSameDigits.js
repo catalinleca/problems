a45ca89a29defbc8d67be7cdada2bc64c98e6eeb
Catalin Leca <leca.catalin1995@gmail.com>
2023-02-12 13:56:41 +0200

    next bigger adj

--- src/nextBiggerSameDigits.js
+++ src/nextBiggerSameDigits.js
@@ -1,20 +1,21 @@
 const sortedDigits = n => {
-  let arr = n.toString().split('');
+  const arr = n.toString().split('');
   arr.sort((a, b) => b - a);
   return arr;
 };
 
 function nextBigger(n) {
-  let arr = sortedDigits(n);
-  let max = parseInt(arr.join(''), 10);
+  const arr = sortedDigits(n);
+  const max = parseInt(arr.join(''), 10);
 
   for(let i = n + 1; i <= max; i++){
     const sortedNumbers = sortedDigits(i)
+    const currentMax = parseInt(sortedNumbers.join(''), 10)
 
-    // check if arrays are identical [3,2,1,0] and [3,2,2,0] -> false
-    const containsAllNumbers = sortedNumbers.every((x, j) => x === arr[j])
+    // const containsAllNumbers = sortedNumbers.every((x, j) => x === arr[j])
 
-    if(containsAllNumbers) {
+    // if same max can be formed, they have the same digits
+    if(currentMax === max) {
       return i;
     }
   }
