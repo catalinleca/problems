5d072352f5f7a68d3cf48bcd2f7fbb54263ccffc
Catalin Leca <34128023+catalinleca@users.noreply.github.com>
2023-02-19 19:09:31 +0200

    lint

--- src/humanReadabletime.js
+++ src/humanReadabletime.js
@@ -1,10 +1,10 @@
 function humanReadable(seconds) {
-  const oneMinute = 60
-  const oneHour = oneMinute * 60
-  const H = ('0' + Math.floor(seconds / oneHour)).slice(-2)
-  const M = ('0' + Math.floor(seconds / oneMinute) % 60).slice(-2)
-  const S = ('0' + seconds % 60).slice(-2)
-  return `${H}:${M}:${S}`
+  const oneMinute = 60;
+  const oneHour = oneMinute * 60;
+  const H = ('0' + Math.floor(seconds / oneHour)).slice(-2);
+  const M = ('0' + Math.floor(seconds / oneMinute) % 60).slice(-2);
+  const S = ('0' + seconds % 60).slice(-2);
+  return `${H}:${M}:${S}`;
 }
 
-console.log(humanReadable(3899))
+console.log(humanReadable(3899));
